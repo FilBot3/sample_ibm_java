d9dd9716db149b412fd14d0eac09b69086dd1c40
Phillip Dudley <Phillip.Dudley@cerner.com>
2015-06-23 15:50:20 -0500

    more and more

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -9,6 +9,6 @@ class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World from IBM Java");
 		System.out.println("Fartfkfyhjetyiryuotuiryt.");
-		System.out.println("Jenkins is annoying. ")
+		System.out.println("Jenkins is annoying. ");
 	}
 }
