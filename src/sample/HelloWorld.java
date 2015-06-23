c868256983159fdccd6f4ef36ff86847f8828a1e
Phillip Dudley <Phillip.Dudley@cerner.com>
2015-06-23 15:00:09 -0500

    Stuff

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -8,6 +8,6 @@ package sample;
 class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World from IBM Java");
-		System.out.println("Hello some more.");
+		System.out.println("Fart.");
 	}
 }
