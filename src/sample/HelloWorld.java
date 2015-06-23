7c6968e4a1ef49e2f3c791d9e5c42f5f0a5136df
Phillip Dudley <Phillip.Dudley@cerner.com>
2015-06-23 15:54:28 -0500

    fiddle fart

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -9,6 +9,5 @@ class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World from IBM Java");
 		System.out.println("Fartfkfyhjetyiryuotuiryt.");
-		System.out.println("Jenkins is annoying. ");
 	}
 }
